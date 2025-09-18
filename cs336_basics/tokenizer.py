c3ccedf62ee308f3d4498e3fef52477ed9e3eddd
Ivan Miao <reneisaipa@gmail.com>
2025-09-18 04:48:37 +0200

    tokenizer pass 20 tests

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -1,5 +1,6 @@
 from typing import Iterable
 import json
+import re
 
 
 class Tokenizer:
@@ -15,6 +16,12 @@ class Tokenizer:
         self.vocab = vocab
         self.merges = merges
         self.special_tokens = special_tokens
+        self.reversed_vocab = {v: k for k, v in self.vocab.items()}
+
+        self.special_pattern = ""
+        if self.special_tokens:
+            self.special_tokens = sorted(self.special_tokens, key=len, reverse=True)
+            self.special_pattern = "|".join(re.escape(st) for st in self.special_tokens)
 
 
     @classmethod
@@ -32,19 +39,54 @@ class Tokenizer:
             vocab_str = json.load(f)
         vocab = {int(k): v.encode('utf-8') for k, v in vocab_str.items()}
 
-        with open(merges_filepath, encoding='utf-8') as f:
-            merges_str = json.load(f)
-        merges = [(p1.encode('utf-8'), p2.encode('utf-8')) for p1, p2 in merges_str]
+        merges = []
+        with open(merges_filepath) as f:
+            for line in f:
+                parts = line.rstrip().split()
+                if len(parts) == 2:
+                    token1, token2 = parts
+                    token_tuple = (token1.encode('utf-8'), token2.encode('utf-8'))
+                    merges.append(token_tuple)
 
         return cls(vocab=vocab, merges=merges, special_tokens=special_tokens)
 
 
     def encode(self, text: str) -> list[int]:
         """Encode an input text into a sequence of token IDs"""
-        text_bytes = text.encode('utf-8')
-        # for b in text_bytes:
-        # TODO
-        return []
+        ids = []
+
+        if self.special_pattern:
+            parts = re.split(f"({self.special_pattern})", text)
+        else:
+            parts = [text]
+
+        for part in parts:
+            if not part:
+                continue
+            if self.special_tokens and part in self.special_tokens:
+                token_id = self.reversed_vocab[part.encode('utf-8')]
+                ids.append(token_id)
+            else:
+                tokens = [bytes([b]) for b in part.encode('utf-8')]
+
+                for p1, p2 in self.merges:
+                    new_tokens = []
+                    i = 0
+                    while i < len(tokens):
+                        if i < len(tokens) - 1 and tokens[i] == p1 and tokens[i+1] == p2:
+                            new_tokens.append(p1 + p2)
+                            i += 2
+                        else:
+                            new_tokens.append(tokens[i])
+                            i += 1
+                    tokens = new_tokens
+
+                for token in tokens:
+                    if token in self.reversed_vocab:
+                        ids.append(self.reversed_vocab[token])
+                    else:
+                        raise ValueError(f"Token {token} not found in vocabulary")
+        return ids
 
 
     def encode_iterable(self, iterable: Iterable[str]) -> Iterable[int]:
@@ -53,11 +95,13 @@ class Tokenizer:
         return a generator that lazily yields token IDs. 
         This is required for memory-eﬀicient tokenization of large files that we cannot directly load into memory
         """
-        # TODO
-        return []
+        for line in iterable:
+            for token_id in self.encode(line):
+                yield token_id
 
 
     def decode(self, ids: list[int]) -> str:
         """Decode a sequence of token IDs into text"""
-        # TODO
-        return ""
+        tokens = b"".join(self.vocab[idx] for idx in ids)
+        text = tokens.decode('utf-8')
+        return text
--- notes/tokenizer.md
+++ notes/tokenizer.md
@@ -5,3 +5,18 @@
 The `from_files()` method is designed to be a class method, which is bound to the class and not the instance of the class. For a class method, we should put a `@classmethod` as a decorator above the method's name.
 
 
+## `json` 库
+
+- `json.loads()`：处理 **内存中** 的 JSON **字符串** (string)。
+- `json.load()`：处理 **文件流** 中的 JSON **数据** (file-like object)。
+
+函数名结尾的 **`s`** 就是 **`string`** 的意思。有 `s` 的处理字符串，没 `s` 的处理文件。
+
+一个json数据可能作为一个字符串变量储存在python代码里，也有可能作为一个.json文件保存在电脑上。json.loads() 和 json.load() 就是分别用来处理这两种情况的。
+
+当你的JSON数据是一个Python字符串时，就用json.loads()
+
+## bytes type
+
+ATTENTION: bytes 在进行迭代的时候，返回的不是bytes而是一个整数！
+
