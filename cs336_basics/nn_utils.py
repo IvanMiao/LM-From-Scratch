e8dafb030922ef005861838ce7c14922b96597e4
Ivan Miao <reneisaipa@gmail.com>
2025-11-09 03:44:16 +0100

    add checkpoint

--- cs336_basics/nn_utils.py
+++ cs336_basics/nn_utils.py
@@ -1,5 +1,7 @@
 import torch
 import einops
+import os
+from typing import IO, BinaryIO
 
 
 def softmax(x: torch.Tensor, i: int) -> torch.Tensor:
@@ -113,3 +115,23 @@ def data_loader(
 
     return (x_batch.to(device), y_batch.to(device))
 
+
+def save_checkpoint(
+        model: torch.nn.Module,
+        optimizer: torch.optim.Optimizer,
+        iteration: int,
+        out: str | os.PathLike | BinaryIO | IO[bytes]
+):
+    checkpoint = {
+        'model_state_dict': model.state_dict(),
+        'optimizer_state_dict': optimizer.state_dict(),
+        'iteration': iteration,
+    }
+    torch.save(checkpoint, out)
+
+
+def load_checkpoint(src, model, optimizer):
+    checkpoint = torch.load(src)
+    model.load_state_dict(checkpoint['model_state_dict'])
+    optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    return checkpoint['iteration']
